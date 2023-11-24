50b115ffbb4e1dcccebeb5cf938e9362c3e60ea9
Ashfakul Islam <r4pido3@gmail.com>
2023-11-24 16:57:02 +0600

    user is now insterting db

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const cors = require('cors');
-const port =  process.env.PORT || 5000
+// middleware for
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+// mongodb server
+
+const uri = process.env.MONGO_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
 
 
-app.get('/', (req,res) => {
-    res.send('Welcome to the Forever Server')
-})
+    // DATABASE COLLECTION
+    const dataBase = client.db("forever_union");
+    const usersCollection = dataBase.collection("users");
 
-app.all('*', (req,res,next) => {
-    const error = new Error (`The requested URL is invalid: ${req.url}`)
-    error.status = 404;
-    next(error);
-})
 
-app.use((err, req, res,next) => {
-    res.status(err.status || 500).json({
-        message: err.message
-    })
-})
+
+    // Users collections
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
+        const query = { email: user.email };
+        const existedUser = await usersCollection.findOne(query);
+        if (existedUser) {
+          return { message: "User already exists" };
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// default routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Forever Server");
+});
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`The requested URL is invalid: ${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`you are listening on ${port}`);
-  });
+  console.log(`you are listening on ${port}`);
+});
